62b8c94f5665d82609322ad80775735cfbc1f826
Brandon Oubre <boubre@mgh.harvard.edu>
2023-11-14 09:54:19 -0500

    Updating local file system binding to reflect new location of gaze codebase

--- env.sh
+++ env.sh
@@ -8,6 +8,6 @@ export PYTHON_IMAGE="/space/neo/4/sif/python_3.10.sif"
 
 # Build up a string of directories that should be exposed to the singularity VM via --bind
 BIND="/space/neo/3/neurobooth/applications/neurobooth-analysis-tools:/dep/neurobooth-analysis-tools"
-BIND="$BIND,/space/neo/3/neurobooth/analyses/bro7_gaze:/dep/gaze_analysis"
+BIND="$BIND,/space/neo/4/analyses/bro7_gaze:/dep/gaze_analysis"
 BIND="$BIND,$PROJECT_DIR/.workspace:/workspace"
 export BIND
